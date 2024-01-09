8f1cbb33c57aa7fc85de8be516ccef14e2ccfaab
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-09 11:10:52 -0500

    Added functions to get, update, and delete products to complete the CRUD functionality

--- src/app/products/product.services.ts
+++ src/app/products/product.services.ts
@@ -6,3 +6,17 @@ export const addProduct = (data: Product)=>{
     products.push(data)
 }
 
+export const getProducts = (): Product[]=>{
+    return products
+}
+
+export const updateProduct = (id: string, changes: Product) =>{
+    const index = products.findIndex((product)=> product.id === id)
+    products[index] = changes
+}
+
+export const deleteProduct = (id: string)=>{
+    const index = products.findIndex((product)=> product.id === id)
+    products.splice(index, 1)
+}
+
